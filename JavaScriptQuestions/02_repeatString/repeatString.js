23cf3b40879c03cc7b01024c6dfdacc15e0b8b1d
Ahmed Shaaban <ashaaban0109@gmail.com>
2023-10-26 08:06:56 +0300

    Completed Basic exercises on looping and arrays

--- JavaScriptQuestions/02_repeatString/repeatString.js
+++ JavaScriptQuestions/02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, numRepeat) {
+    if (numRepeat < 0) {
+        return "ERROR"
+    }
+    
+    let combined = ""
+    for (let i = 0; i < numRepeat; i++) {
+        combined += string;
+    }
+    return combined
 };
 
 // Do not edit below this line
--- JavaScriptQuestions/05_sumAll/sumAll.js
+++ JavaScriptQuestions/05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(...twoNums) {
+    if (typeof twoNums[0] !== 'number' || typeof twoNums[1] !=='number') {
+        return "ERROR"
+    }
 
+    if (twoNums[0]<0 || twoNums[1] <0) {
+        return "ERROR"
+    }
+    let start=twoNums[0]
+    let end =twoNums[1]
+    if (twoNums[0] > twoNums[1]) {
+        start = twoNums[1]
+        end = twoNums[0]
+    }
+
+    let commulativeSum = 0
+    for (let n = start; n <= end; n++) {
+        commulativeSum += n
+    }
+    return commulativeSum
 };
 
 // Do not edit below this line
